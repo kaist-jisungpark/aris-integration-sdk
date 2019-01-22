e4033cc45c1da64f4cf370a4dbbb42bd6c9dd0ba
Curt Nichols <curt.nichols@live.com>
2019-01-22 12:53:53 -0800

    Initial, unfiltered functionality

--- common/platform-dotnet/aris-tools/extract-frame-headers/ArisFile.cs
+++ common/platform-dotnet/aris-tools/extract-frame-headers/ArisFile.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -11,33 +13,100 @@ namespace extract_frame_headers
     {
         private readonly Stream stream;
         private readonly int frameCount;
+        private readonly int fullFrameSize;
 
         private bool disposed;
 
-        public static (bool, ArisFile) Create(string path)
+        public static bool Create(string path, out ArisFile arisFile)
         {
             try
             {
                 var stream = File.OpenRead(path);
-                if (CheckFile(stream))
+                if (CheckFile(stream) && CountFrames(stream, out var frameCount, out var frameSize))
                 {
-                    return (true, new ArisFile(stream));
+                    arisFile = new ArisFile(stream, frameCount, frameSize);
+                    return true;
                 }
                 else
                 {
-                    return (false, null);
+                    arisFile = null;
+                    return false;
                 }
             }
             catch (IOException)
             {
-                return (false, null);
+                arisFile = null;
+                return false;
             }
         }
 
-        private ArisFile(Stream stream)
+        public void ExportCsv(TextWriter output)
+        {
+            var fields = GetFieldInfos(new HashSet<string>()).ToArray();
+            ExportColumnHeaders(fields, output);
+
+            for (int i = 0; i < int.MaxValue; ++i)
+            {
+                if (ReadFrameHeaderByIndex(i, out var frameHeader))
+                {
+                    ExportFrameHeaderFields(fields, output, frameHeader);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void ExportColumnHeaders(FieldInfo[] fis, TextWriter output)
+        {
+            var buf = new StringBuilder();
+            bool isFirst = true;
+
+            foreach (var fi in fis)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                }
+                else
+                {
+                    buf.Append(",");
+                }
+
+                buf.Append(fi.Name);
+            }
+
+            output.WriteLine(buf.ToString());
+        }
+
+        private void ExportFrameHeaderFields(FieldInfo[] fis, TextWriter output, object boxedHeader)
+        {
+            var buf = new StringBuilder();
+            bool isFirst = true;
+
+            foreach (var fi in fis)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                }
+                else
+                {
+                    buf.Append(",");
+                }
+
+                buf.Append(fi.GetValue(boxedHeader).ToString());
+            }
+
+            output.WriteLine(buf.ToString());
+        }
+
+        private ArisFile(Stream stream, int frameCount, int frameSize)
         {
             this.stream = stream;
-            frameCount = CountFrames(stream);
+            this.frameCount = frameCount;
+            this.fullFrameSize = frameSize;
         }
 
         ~ArisFile()
@@ -47,11 +116,11 @@ namespace extract_frame_headers
 
         public int FrameCount { get { return frameCount; } }
 
-        public bool ReadFileHeader(out ArisFileHeader header) =>
+        private bool ReadFileHeader(out ArisFileHeader header) =>
             ReadFileHeader(stream, out header);
 
-        public bool ReadFrameHeaderByIndex(int index, ref ArisFrameHeader header) =>
-            ReadFrameHeaderByIndex(stream, index, out header);
+        private bool ReadFrameHeaderByIndex(int index, out ArisFrameHeader header) =>
+            ReadFrameHeaderByIndex(stream, index, fullFrameSize, out header);
 
 
         private static bool CheckFile(Stream stream)
@@ -68,25 +137,94 @@ namespace extract_frame_headers
 
             if (!ReadFileHeader(stream, out fileHeader))
             {
+                Console.Error.WriteLine("Couldn't read the file header");
+                return false;
             }
 
-            throw new Exception();
+            if (fileHeader.Version != ArisFileHeader.ArisFileSignature)
+            {
+                Console.Error.WriteLine("File is not an ARIS file");
+                return false;
+            }
+
+            return true;
         }
 
-        private static int CountFrames(Stream stream)
+        private static uint PingModeToNumBeams(uint pingMode)
         {
-            throw new Exception();
+            if (pingMode == 1)
+            {
+                return 48;
+            }
+            else if (pingMode == 3)
+            {
+                return 96;
+            }
+            else if (pingMode == 6)
+            {
+                return 64;
+            }
+            else if (pingMode == 9)
+            {
+                return 128;
+            }
+
+            throw new ArgumentOutOfRangeException("pingMode");
+        }
+
+        private static bool CountFrames(Stream stream, out int frameCount, out int frameSize)
+        {
+            if (ReadAt<ArisFrameHeader>(stream, Marshal.SizeOf<ArisFileHeader>(), out var frameHeader)
+                && frameHeader.Version == ArisFrameHeader.ArisFrameSignature)
+            {
+                var beamCount = PingModeToNumBeams(frameHeader.PingMode);
+                var sampleCount = frameHeader.SamplesPerBeam;
+                var frameDataSize = beamCount * sampleCount;
+                var fullFrameSize = (int)frameDataSize + Marshal.SizeOf<ArisFrameHeader>();
+                var availableFrameData = stream.Length - Marshal.SizeOf<ArisFileHeader>();
+                frameCount = (int)(availableFrameData / fullFrameSize);
+                frameSize = fullFrameSize;
+                return true;
+            }
+            else
+            {
+                Console.Error.WriteLine("Bad frame header");
+                frameCount = 0;
+                frameSize = 0;
+                return false;
+            }
         }
 
         private static bool ReadFileHeader(Stream stream, out ArisFileHeader header) =>
-            ReadAt<ArisFileHeader>(0L, out header);
+            ReadAt<ArisFileHeader>(stream, 0L, out header);
 
-        private static bool ReadFrameHeaderByIndex(Stream stream, int index, out ArisFrameHeader header) =>
-            throw new Exception();
-
-        private static bool ReadAt<T>(long position, out T t)
+        private static bool ReadFrameHeaderByIndex(Stream stream, int index, int fullFrameSize, out ArisFrameHeader header)
         {
-            throw new Exception();
+            var pos = Marshal.SizeOf<ArisFileHeader>() + (index * fullFrameSize);
+
+            return ReadAt<ArisFrameHeader>(stream, pos, out header);
+        }
+
+        private static bool ReadAt<T>(Stream stream, long position, out T t)
+        {
+            var buf = new byte[Marshal.SizeOf<T>()];
+
+            if (stream.Seek(position, SeekOrigin.Begin) != position
+                || stream.Read(buf, 0, buf.Length) != buf.Length)
+                    {
+                t = default(T);
+                return false;
+            }
+
+            unsafe
+            {
+                fixed (byte* pB = buf)
+                {
+                    var newValue = Marshal.PtrToStructure<T>(new IntPtr(pB));
+                    t = newValue;
+                    return true;
+                }
+            }
         }
 
         public void Dispose()
@@ -111,6 +249,27 @@ namespace extract_frame_headers
             }
 
             // Dispose non-managed resources
+        }
+
+        private static IEnumerable<string> GetOrderedFrameHeaderFieldNames()
+        {
+            foreach (var f in typeof(ArisFrameHeader).GetFields().Where(f => !f.IsStatic && f.Name != "padding"))
+            {
+                yield return f.Name;
+            }
+        }
+
+        private static IEnumerable<FieldInfo> GetFieldInfos(HashSet<string> filter)
+        {
+            bool useAll = filter.Count == 0;
+
+            var t = typeof(ArisFrameHeader);
+            var d = new Dictionary<string, FieldInfo>();
+
+            foreach (var name in GetOrderedFrameHeaderFieldNames().Where(name => useAll || filter.Contains(name)))
+            {
+                yield return t.GetField(name);
+            }
         }
     }
 }
--- common/platform-dotnet/aris-tools/extract-frame-headers/Program.cs
+++ common/platform-dotnet/aris-tools/extract-frame-headers/Program.cs
@@ -1,6 +1,5 @@
-﻿using Aris.FileTypes;
-using System;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.IO;
 
 namespace extract_frame_headers
 {
@@ -8,8 +7,53 @@ namespace extract_frame_headers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            Arguments arguments;
+            if (ParseArguments(args, out arguments))
+            {
+                if (ArisFile.Create(arguments.FilePath, out var arisFile))
+                {
+                    using (var api = arisFile)
+                    {
+                        api.ExportCsv(Console.Out);
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Couldn't process file '${arguments.FilePath}'");
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Invalid or missing arguments.");
+                ShowUsage(Console.Error);
+                Environment.Exit(1);
+            }
         }
+
+        private static bool ParseArguments(string[] clArgs, out Arguments args)
+        {
+            if (clArgs.Length == 0)
+            {
+                args = null;
+                return false;
+            }
+
+            var filePath = clArgs[0];
+            var fieldsOfInterest = new string[0];
+
+            args = new Arguments { FilePath = filePath, FieldsOfInterest = fieldsOfInterest };
+            return true;
+        }
+
+        private static void ShowUsage(TextWriter output)
+        {
+            output.WriteLine("USAGE: extract-frame-headers <aris-recording-path>");
+        }
+    }
+
+    internal class Arguments
+    {
+        public string FilePath { get; set; }
+        public string[] FieldsOfInterest { get; set; }
     }
 }
